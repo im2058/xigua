5d5bb5698a6e5528f1ee9e796e373f76002a6189
im2058 <10438075@qq.com>
2018-10-21 09:11:16 +0000

    add init.sh

--- config/init.sh
+++ config/init.sh
@@ -6,3 +6,8 @@ sudo service smbd restart
 sudo chmod -R 777 ../download
 sudo chmod -R 777 ../source
 sudo chmod 755 ../download/update.log
+#crontab -e 
+#*/30 * * * * cd /home/pi/KK/xigua && bash xigua.sh > tmp/cron.log
+
+usb=`ls /dev/sd*`
+sudo mount -t vfat -o umask=000 $usb /home/pi/KK/xigua/download
