27808ce58808d42d7111f9db1e6a91a09fbdba37
im2058 <10438075@qq.com>
2018-10-21 03:45:12 +0000

    add phantomjs

--- init.sh
+++ init.sh
@@ -3,3 +3,8 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get install -y vim tmux samba phantomjs python3-selenium
+sudo apt-get install -y python3-dev build-essential libssl-dev libffi-dev libxml2 libxml2-dev libxslt1-dev zlib1g-dev libcurl4-openssl-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install pyquery
+
+git clone https://github.com/piksel/phantomjs-raspberrypi.git
