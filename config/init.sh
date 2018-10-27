eae51513ff6075aba5eb9a4a96171dc2a4d13f23
im2058 <10438075@qq.com>
2018-10-27 02:21:41 +0000

    m

--- config/init.sh
+++ config/init.sh
@@ -6,6 +6,8 @@ sudo service smbd restart
 sudo chmod -R 777 ../download
 sudo chmod -R 777 ../source
 sudo chmod 755 ../download/update.log
+ipa=`ifconfig | grep "broadcast" | sed 's/inet\(.*\)netmas.*/\1/g'`
+python ../py_script/kk.py "$ipa" "source_and_download dir"
 #crontab -e 
 #*/30 * * * * cd /home/pi/KK/xigua && bash xigua.sh > tmp/cron.log
 
